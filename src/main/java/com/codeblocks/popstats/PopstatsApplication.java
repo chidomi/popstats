d406bd6fdf058ebf3ab575184f95670b38bbab17
chidomi <chidomi1990@gmail.com>
2021-01-03 15:45:23 -0500

    start a project

---
+++ src/main/java/com/codeblocks/popstats/PopstatsApplication.java
@@ -0,0 +1,13 @@
+package com.codeblocks.popstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopstatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeblocks/popstats/PopstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeblocks.popstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
